00e7c372941befacd0a2fc3d94e2afb5faf59f80
lainbo <46062895+lainbo@users.noreply.github.com>
2022-02-09 22:15:26 +0800

    fix: 修复路径

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import viteCompression from 'vite-plugin-compression'
 export default defineConfig({
-  base: '/naptime_vite/',
+  base: '/naptime/',
   plugins: [WindiCSS(), vue(), viteCompression()],
 })
